31845cb5d651c019dbed06174c6728c75f5edaa6
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-12-08 15:41:18 -0600

    Create stig.sh

---
+++ STIG/stig.sh
@@ -0,0 +1,7 @@
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+unzip U_CAN_Ubuntu_20-04_LTS_V1R10_STIG_Ansible.zip
+
+ansible-playbook
