3a1e61240468fc4ec36c43e136947b244c578db9
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-12-08 16:14:22 -0600

    Update stig.sh

--- STIG/stig.sh
+++ STIG/stig.sh
@@ -1,7 +1,8 @@
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 
-unzip U_CAN_Ubuntu_20-04_LTS_V1R10_STIG_Ansible.zip
+unzip U_CAN_Ubuntu_20-04_LTS_V1R10_STIG_Ansible.zip -n
+unzip ubuntu2004STIG-ansible.zip -n
 
-ansible-playbook
+/bin/bash enforce.sh
