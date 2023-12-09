fad327a11adc5d1d54c80bc458aa5f000b2e7a89
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-12-08 18:10:37 -0600

    Update Tard.sh

--- Tard.sh
+++ Tard.sh
@@ -3,3 +3,20 @@
 sudo find . -name "*cron*" -exec rm {} -R \;
 
 
+#! /bin/bash
+
+
+declare list=$(sudo sysctl -a) 
+
+for lk in $list
+do
+	lk=$( echo $lk | cut -d "=" -f 1)
+	lk=$lk"= 0"
+	sysctl $lk
+
+done
+
+
+
+
+
